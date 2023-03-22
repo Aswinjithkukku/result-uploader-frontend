427fb27eceb9bf41d80c2d8fd2cad462ecd81ae9
ilyasbabu <ilyasbabuvp@gmail.com>
2023-03-22 11:43:30 +0530

    student list page

---
+++ src/pages/StudentListPage.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+
+function StudentListPage() {
+    return (
+        <div>
+            <div className="container p-2 mx-auto sm:p-4 dark:dark:text-gray-100">
+                <div className='flex justify-between items-center pr-9'>
+                    <h2 className="mb-4 text-2xl pl-9 pb-5 font-semibold leading-tight">Student List - Faculty Name</h2>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Student</button>
+                </div>
+
+                <div className="overflow-x-auto px-11 flex min-h-[55vh] justify-center items-center">
+                    <div class="relative overflow-x-auto w-full">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Register Number
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Student Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <p>&nbsp;</p>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS034
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS036
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS045
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS045
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS045
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS045
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS045
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        AZATSCS045
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Student name KP
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Result</button>
+                                    </td>
+                                </tr>
+
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StudentListPage
