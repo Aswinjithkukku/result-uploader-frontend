8027158ea406a8c02a71422ddde9bda886633d53
ilyasbabu <ilyasbabuvp@gmail.com>
2023-03-23 11:56:19 +0530

    marksheet uploade page done

--- src/pages/MarkSheetUpload.jsx
+++ src/pages/MarkSheetUpload.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "../axios";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { keyboardImplementationWrapper } from '@testing-library/user-event/dist/keyboard';
 import Swal from 'sweetalert2';
 
 function MarkSheetUpload() {
@@ -13,6 +11,7 @@ function MarkSheetUpload() {
     const [loader, setLoader] = useState(false);
     const [exam, setExam] = useState('');
     const [file, setFile] = useState();
+    const [disabled, setDisabled] = useState(true);
 
 
 
@@ -25,7 +24,6 @@ function MarkSheetUpload() {
                 },
             });
             setExams(response?.data);
-            console.log(exams);
             setIsLoading(false);
         } catch (err) {
             console.log(err);
@@ -45,7 +43,7 @@ function MarkSheetUpload() {
                     'authorization': `Token ${token}`
                 }
             })
-            Swal.fire("Good job!", "You clicked the button!", "success");
+            Swal.fire("Good job!", "Marksheet Uploaded Successfully!", "success");
             setLoader(false)
         } catch (err) {
             setError(err?.response?.data[0])
@@ -64,20 +62,20 @@ function MarkSheetUpload() {
                 <div className="overflow-x-auto px-11 flex min-h-[55vh] justify-center items-center">
                     <form action="" className='w-full ' onSubmit={handleSubmit}>
                         <div className='pb-9'>
-                            <label for="exams" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Examination:</label>
-                            <select value={exam} onChange={(e) => setExam(e.target.value)} id="exams" class=" bg-blue-50 border border-blue-700 text-gray-900 text-sm rounded-lg focus:ring-blue-800 focus:border-blue-800 block w-1/2 p-2.5 ">
-                                <option selected>Choose a Exam</option>
+                            <label htmlFor="exams" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Examination:</label>
+                            <select value={exam} onChange={(e) => setExam(e.target.value)} id="exams" className=" bg-blue-50 border border-blue-700 text-gray-900 text-sm rounded-lg focus:ring-blue-800 focus:border-blue-800 block w-1/2 p-2.5 ">
+                                <option value="" >Choose a Exam</option>
                                 {
                                     exams?.map((exam, index) => (
-                                        <option value={exam.id}>{exam.exam_name}</option>
+                                        <option key={index} value={exam.id}>{exam.exam_name}</option>
                                     ))
                                 }
                             </select>
                         </div>
                         <div className='pb-9'>
-                            <label for="marksheet" class="block mb-2 text-sm font-medium text-gray-900 ">Upload Marksheet:</label>
+                            <label htmlFor="marksheet" className="block mb-2 text-sm font-medium text-gray-900 ">Upload Marksheet:</label>
                             <label>
-                                <input type="file" onChange={(e) => setFile(e.target.files[0])} accept=".pdf" class="text-sm text-grey-500 
+                                <input type="file" onChange={(e) => setFile(e.target.files[0])} accept=".pdf" className="text-sm text-grey-500 
                                     file:mr-5 file:py-2 file:px-6
                                     file:rounded-lg file:border-0
                                     file:text-sm file:font-medium
@@ -86,29 +84,39 @@ function MarkSheetUpload() {
                                     hover:file:text-blue-800
                                 " />
                             </label>
-                            <p class="mt-4 ml-2 text-sm text-gray-400 dark:text-gray-300" id="file_input_help">
+                            <p className="mt-4 ml-2 text-sm text-gray-400 dark:text-gray-300" id="file_input_help">
                                 Upload PDF File of the result from University Of Calicut Portal
                             </p>
                         </div>
-                        <div class="flex items-center pb-5">
-                            <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2" />
-                            <label for="link-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I accept the above given Document and details are true.</label>
+                        <div className="flex items-center pb-5">
+                            <input id="link-checkbox" type="checkbox" value="" onChange={() => setDisabled(!disabled)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2" />
+                            <label htmlFor="link-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I accept the above given Document and details are true.</label>
                         </div>
-                        { error &&
+                        {error &&
                             <div>
                                 <p className='text-sm text-red-600 pb-3'>
                                     {error}
                                 </p>
                             </div>
                         }
-                        <button type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <button type="submit" disabled={disabled}
+                            className={`${disabled ? "cursor-not-allowed bg-blue-300 hover:bg-blue-300/80" : "cursor-pointer bg-blue-700 hover:bg-blue-800"} text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-6 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}>
                             Upload
                         </button>
                         <span
-                            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                             Cancel
                         </span>
+                        {
+                            loader &&
+                            <div role="status py-10">
+                                <svg aria-hidden="true" className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                                <span className="sr-only">Loading...</span>
+                            </div>
+                        }
                     </form>
                 </div>
             </div>
